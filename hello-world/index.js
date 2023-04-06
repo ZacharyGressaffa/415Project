435ac976ba223359eeae63f1f55d31ae96fc833d
Zachary Gressaffa <zachary.gressaffa@selu.edu>
2023-04-06 03:20:27 -0500

    render test

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,17 +1,68 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
 
-router.get('/test', function(req, res) {
-    res.status(200).send('Hello world');
+const app = express();
+const port = 3000;
+
+// Set the view engine to use Pug
+app.set('view engine', 'pug');
+
+// Middleware to handle form data
+app.use(express.urlencoded({ extended: true }));
+
+// Homepage route
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
 });
 
-app.use('/api', router);
+// Create user ID route
+app.post('/create-user', (req, res) => {
+    const userId = req.body.userId;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const user = {
+        userId: userId,
+        firstName: firstName,
+        lastName: lastName
+    };
+    fs.readFile('mydata.txt', (err, data) => {
+        let users = [];
+        if (!err) {
+            users = JSON.parse(data);
+        }
+        users.push(user);
+        fs.writeFile('mydata.txt', JSON.stringify(users), err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.redirect('/');
+        });
+    });
+});
 
-app.listen(1069, function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Magic Happens on Port 69');
-    }
+// Search for user ID route
+app.post('/search-user', (req, res) => {
+    const userId = req.body.userId;
+    fs.readFile('mydata.txt', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        let users = JSON.parse(data);
+        let user = users.find(user => user.userId === userId);
+        if (user) {
+            res.render('user', { title: 'User', user: user });
+        } else {
+            res.render('user', { title: 'User', error: 'User ID not found' });
+        }
+    });
+});
+
+// Create mydata.txt file if it doesn't exist
+if (!fs.existsSync('mydata.txt')) {
+    fs.writeFileSync('mydata.txt', '[]');
+}
+
+app.listen(port, () => {
 });
